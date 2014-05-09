4a0d56f11593b25062c09501e2c71d679a00d307
crsdrw <chris@cdrew.co.uk>
2014-05-09 14:49:11 +0100

    Cleanup

--- SandBox/StatsFinder.h
+++ SandBox/StatsFinder.h
@@ -1,34 +1,37 @@
 #pragma once
 #include "DataStats.h"
 
-template<typename T, class D>
+template <typename T, class D>
 class DataValue {
 };
 
-template<class D>
+template <class D>
 class DataValue<int, D> {
- public:
+public:
   static inline int getValue(const D& data, int index) { return data.getInt(index); }
 };
 
-template<class D>
-class DataValue<float, D> {
- public:
+template <class D>
+class DataValue<float, D > {
+public:
   static inline float getValue(const D& data, int index) { return data.getFloat(index); }
 };
 
-template<typename T, class D>
+template <typename T, class D>
 class StatsFinder {
+private:
+  const D& data_;
 public:
-  static DataStats find(const D& data);
+  StatsFinder(const D& data) : data_(data) {}
+  DataStats find() const;
+  StatsFinder& operator=(const StatsFinder&) = delete;
 };
 
-template<typename T, class D> DataStats
-StatsFinder<T, D>::find(const D& data) {
+template <typename T, class D> DataStats
+StatsFinder<T, D>::find() const {
   DataStats data_stats;
-  int data_size = data.getSize();
-  for (int i = 0; i != data_size; ++i) {
-    data_stats.addValue(DataValue<T, D>::getValue(data, i));
-  }
+  int data_size = data_.getSize();
+  for (int i = 0; i != data_size; ++i)
+    data_stats.addValue(DataValue<T, D>::getValue(data_, i));
   return data_stats;
 }
